9d163dc9878dd28dca813f887c559f74a06a65eb
wander-junior <wanderjrcruz96@gmail.com>
2025-05-22 17:04:42 -0300

    feat: read port from environment

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
+import os
 
 app = Flask(__name__)
 
@@ -43,4 +44,5 @@ def classify():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
